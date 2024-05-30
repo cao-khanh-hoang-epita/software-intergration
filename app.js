5c016935d18b3a14c5ada9e91b525f3d29f59dae
Khanh <hoangcaokhanhhn2004@gmail.com>
2024-05-30 10:52:54 +0200

    ok
    ok

--- app.js
+++ app.js
@@ -22,52 +22,7 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-// Create
-app.post('/users', async (req, res) => {
-    const user = new User(req.body);
-    try {
-        await user.save();
-        res.status(201).send(user);
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
 
-// Read
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find();
-        res.send(users);
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
-
-// Update
-app.put('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
-    } catch (err) {
-        res.status(400).send(err);
-    }
-});
-
-// Delete
-app.delete('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id);
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
-    } catch (err) {
-        res.status(500).send(err);
-    }
-});
 
 app.listen(config.PORT, () => {
     console.log(`Server is running on port ${config.PORT}`);
